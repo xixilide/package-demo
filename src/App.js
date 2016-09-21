5156cfcfee5be43daab209131954b1202b5f3712
xixilide <754396775@qq.com>
2016-09-21 09:08:39 +0800

    finese

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import NavHeader from './component/NavHeader';
 import NavFooter from './component/NavFooter';
+import NavLeft from './component/NavLeft';
 
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      showNav:false
+    }
+  }
+  setNavBarState(){
+    this.setState({
+      showNav:window.innerWidth > 760 ? true : false
+    });
+  }
+  componentDidMount(){
+    this.setNavBarState();
+    window.onresize = this.setNavBarState.bind(this);
+  }
   render () {
     return(
       <div className="content-warp">
-        <NavHeader />
+        {this.state.showNav ? <NavLeft /> : <NavHeader />}
+
         <div className="content-main">
           {this.props.children}
         </div>
+          {this.state.showNav ? "" : <NavFooter />}
 
-        <NavFooter />
       </div>
     )
   }
--- src/component/NavHeader.js
+++ src/component/NavHeader.js
@@ -4,7 +4,13 @@ class NavHeader extends React.Component {
   render () {
   return(
     <div className="nav-header">
-    i am header
+  <p>
+  <span className="glyphicon glyphicon-arrow-left"></span>back</p>
+
+    <h3>xixilide@home</h3>
+
+  <span className="glyphicon glyphicon-record"></span>
+
     </div>
   )
   }
