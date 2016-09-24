da8d4106db30c2121c61b18dfaed9f1bc8b9005f
xixilide <754396775@qq.com>
2016-09-24 08:26:05 +0800

    too

--- src/component/abouthobby.js
+++ src/component/abouthobby.js
@@ -8,7 +8,12 @@ class Hobby extends React.Component {
   render () {
     return(
       <div>
-        hobby
+        <ul class="list-group">
+            <li class="list-group-item list-group-item-success">健 身</li>
+            <li class="list-group-item list-group-item-info">写代码</li>
+            <li class="list-group-item list-group-item-warning">跳 舞</li>
+            <li class="list-group-item list-group-item-danger">看 书</li>
+        </ul>
       </div>
     )
   }
