1a8bfbb334c1d94aee21ecf1cc7657c735001a21
xixilide <754396775@qq.com>
2016-09-25 19:35:07 +0800

    in

--- src/component/abouthobby.js
+++ src/component/abouthobby.js
@@ -8,11 +8,12 @@ class Hobby extends React.Component {
   render () {
     return(
       <div>
-        <ul class="list-group">
-            <li class="list-group-item list-group-item-success">健 身</li>
-            <li class="list-group-item list-group-item-info">写代码</li>
-            <li class="list-group-item list-group-item-warning">跳 舞</li>
-            <li class="list-group-item list-group-item-danger">看 书</li>
+        <h3>我的爱好</h3>
+        <ul className="list-group">
+            <li className="list-group-item list-group-item-success">健 身</li>
+            <li className="list-group-item list-group-item-info">写代码</li>
+            <li className="list-group-item list-group-item-warning">跳 舞</li>
+            <li className="list-group-item list-group-item-danger">看 书</li>
         </ul>
       </div>
     )
--- src/work/work.js
+++ src/work/work.js
@@ -7,7 +7,7 @@ class Work extends React.Component {
 
 
         <div className="work-card">
-              <a href="#"><img src="http://img1.skqkw.cn:888/2014/11/10/12t/li0bekpma55-23603.jpg" /></a>
+              <a href="xixilide.github.io"><img src="http://img1.skqkw.cn:888/2014/11/10/12t/li0bekpma55-23603.jpg" /></a>
                   <div className="work-text">
                     <h3>作品一</h3>
                     <p>2016.5.6</p>
@@ -15,7 +15,7 @@ class Work extends React.Component {
                 </div>
 
       <div className="work-card">
-            <a href="#"><img src="http://img4.duitang.com/uploads/item/201510/18/20151018120957_W3AGU.jpeg" /></a>
+            <a href="xixilide.github.io"><img src="http://img4.duitang.com/uploads/item/201510/18/20151018120957_W3AGU.jpeg" /></a>
                 <div className="work-text">
                   <h3>作品二</h3>
                   <p>2015.10.6</p>
@@ -23,7 +23,7 @@ class Work extends React.Component {
               </div>
 
     <div className="work-card">
-          <a href="#"><img src="http://img0.imgtn.bdimg.com/it/u=3983480950,1985560014&fm=21&gp=0.jpg" /></a>
+          <a href="xixilide.github.io"><img src="http://img0.imgtn.bdimg.com/it/u=3983480950,1985560014&fm=21&gp=0.jpg" /></a>
               <div className="work-text">
                 <h3>作品三</h3>
                 <p>2016.1.5</p>
@@ -31,7 +31,7 @@ class Work extends React.Component {
     </div>
 
   <div className="work-card">
-        <a href="#"><img src="http://img5.duitang.com/uploads/item/201507/15/20150715012629_LQ4T5.jpeg" /></a>
+        <a href="xixilide.github.io"><img src="http://img5.duitang.com/uploads/item/201507/15/20150715012629_LQ4T5.jpeg" /></a>
           <div className="work-text">
               <h3>作品一</h3>
               <p>2016.4.6</p>
